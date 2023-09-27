e6f3b0412ef02762be22ebd594098a2fe304dff7
Sun <jumbooo0307@gmail.com>
2023-09-27 16:14:28 +0800

    hibernate架構建立、ignore新增bin和hibernate組態檔、資料庫更新

--- src/main/java/com/tha103/jo/HelloSun.java
+++ src/main/java/com/tha103/jo/HelloSun.java
@@ -1,7 +1,34 @@
 package com.tha103.jo;
 
+import org.hibernate.Session;
+
+import entity.Webempadmin;
+import util.HibernateUtil;
+
+
 public class HelloSun {
-	private String dog;
-	private String cat;
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			
+			Webempadmin emp = new Webempadmin();
+			emp.setEmpName("aaa");
+			emp.setEmpPassword("1234");
+			emp.setEmpArriveDate(java.sql.Date.valueOf("2005-01-01"));
+			emp.setEmpAdminAuthorization("ceo");
+			
+			session.save(emp);
+			
+			session.getTransaction().commit();
+			
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		} finally {
+			HibernateUtil.shutdown();
+		}	
+		
+	}
 
 }
