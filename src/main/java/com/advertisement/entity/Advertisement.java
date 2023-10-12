7712b9cbe39378e492ddc3b9b7845f93ae79e06d
Wenkar Wang <windgofast999@gmail.com>
2023-10-13 00:19:22 +0800

    更新Advertisement的entity，新增String advertisementName欄位，測試完畢

--- src/main/java/com/advertisement/entity/Advertisement.java
+++ src/main/java/com/advertisement/entity/Advertisement.java
@@ -32,6 +32,9 @@ public class Advertisement {
 	@Column(name="advertisementBlob", columnDefinition = "longBlob" )
 	private byte[] advertisementBlob;
 	
+	@Column(name="advertisementName")
+	private String advertisementName;
+	
 	@Column(name="advertisementUpTime")
 	private Timestamp advertisementUpTime;
 	
@@ -48,86 +51,104 @@ public class Advertisement {
 	public Advertisement() {
 		super();
 	}
-	
-	
 
-	public Advertisement(Integer advertisementID, Integer dinerID, byte[] advertisementBlob,
+
+	public Advertisement(Integer advertisementID, Integer dinerID, byte[] advertisementBlob, String advertisementName,
 			Timestamp advertisementUpTime, Timestamp advertisementDownTime, Integer advertisementDuringTime,
 			String advertisementStatus) {
 		super();
 		this.advertisementID = advertisementID;
 		this.dinerID = dinerID;
 		this.advertisementBlob = advertisementBlob;
+		this.advertisementName = advertisementName;
 		this.advertisementUpTime = advertisementUpTime;
 		this.advertisementDownTime = advertisementDownTime;
 		this.advertisementDuringTime = advertisementDuringTime;
 		this.advertisementStatus = advertisementStatus;
 	}
-
 
 
 	public Integer getAdvertisementID() {
 		return advertisementID;
 	}
 
+
 	public void setAdvertisementID(Integer advertisementID) {
 		this.advertisementID = advertisementID;
 	}
+
 
 	public Integer getDinerID() {
 		return dinerID;
 	}
 
+
 	public void setDinerID(Integer dinerID) {
 		this.dinerID = dinerID;
 	}
+
 
 	public byte[] getAdvertisementBlob() {
 		return advertisementBlob;
 	}
 
+
 	public void setAdvertisementBlob(byte[] advertisementBlob) {
 		this.advertisementBlob = advertisementBlob;
 	}
+
+
+	public String getAdvertisementName() {
+		return advertisementName;
+	}
+
+
+	public void setAdvertisementName(String advertisementName) {
+		this.advertisementName = advertisementName;
+	}
+
 
 	public Timestamp getAdvertisementUpTime() {
 		return advertisementUpTime;
 	}
 
+
 	public void setAdvertisementUpTime(Timestamp advertisementUpTime) {
 		this.advertisementUpTime = advertisementUpTime;
 	}
+
 
 	public Timestamp getAdvertisementDownTime() {
 		return advertisementDownTime;
 	}
 
+
 	public void setAdvertisementDownTime(Timestamp advertisementDownTime) {
 		this.advertisementDownTime = advertisementDownTime;
 	}
+
 
 	public Integer getAdvertisementDuringTime() {
 		return advertisementDuringTime;
 	}
 
+
 	public void setAdvertisementDuringTime(Integer advertisementDuringTime) {
 		this.advertisementDuringTime = advertisementDuringTime;
 	}
+
 
 	public String getAdvertisementStatus() {
 		return advertisementStatus;
 	}
 
+
 	public void setAdvertisementStatus(String advertisementStatus) {
 		this.advertisementStatus = advertisementStatus;
 	}
+	
+	
 
-	@Override
-	public String toString() {
-		return "Advertisement [advertisementID=" + advertisementID +  ", advertisementBlob="
-				+ Arrays.toString(advertisementBlob) + ", advertisementUpTime=" + advertisementUpTime
-				+ ", advertisementDownTime=" + advertisementDownTime + ", advertisementDuringTime="
-				+ advertisementDuringTime + ", advertisementStatus=" + advertisementStatus + "]";
-	}
+
 
 }
