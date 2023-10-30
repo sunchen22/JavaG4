e1d61df98f9222a09ae62d044505da1d3f3d0959
Sun <jumbooo0307@gmail.com>
2023-10-30 19:15:00 +0800

    usernews fix bug

--- src/main/java/com/webempadmin/model/WebempadminDAO_interface.java
+++ src/main/java/com/webempadmin/model/WebempadminDAO_interface.java
@@ -9,6 +9,7 @@ public interface WebempadminDAO_interface {
           public void suspend(Integer empID);
           public WebempadminVO findByPrimaryKey(Integer empID);
           public Map<String,String> findAccoundPassword(String empName);
+          public int findEmpID (String empName);
           public Map<String,Integer> findEmpStatus(String empName);
           public List<WebempadminVO> getAll();
           //萬用複合查詢(傳入參數型態Map)(回傳 List)
